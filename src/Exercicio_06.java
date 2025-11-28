c30509cc7470c2e8a54ada97aecb92ffd8b23a34
José Froes <josepaulo.froes@gmail.com>
2025-11-28 18:36:41 -0300

    Crie um LocalDateTime com data e hora atuais.

---
+++ src/Exercicio_06.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Exercicio_06 {
+//    Crie um LocalDateTime com data e hora atuais.
+    public static void main(String[] args) {
+        LocalDateTime dataHoraAtual = LocalDateTime.now();
+
+        System.out.println(dataHoraAtual);
+
+
+
+    }
+}
