c641c95b8fa79396d038a648b477f5a33d2fbf61
José Froes <josepaulo.froes@gmail.com>
2025-11-28 18:19:05 -0300

    Crie um LocalDate com a data atual.

---
+++ src/Exercicio_01.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Exercicio_01 {
+//    Crie um LocalDate com a data atual.
+    public static void main(String[] args) {
+        LocalDate data = LocalDate.now();
+
+        System.out.println(data);
+    }
+}
